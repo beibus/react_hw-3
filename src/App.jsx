05f60ae779dacedbba7f7d18f2486475beb07c6c
Ulukbek <orozgozhoev.u@gmail.com>
2022-08-12 14:25:26 +0600

    added react route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css';
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import Details from './pages/Details'
+import Promo from './pages/Promo'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="home" element={<Home />}/>
+        <Route path="details" element={<Details />}/>
+        <Route path="promo" element={<Promo />}/>
+        </Routes >
+        <Footer/>
     </div>
   );
 }
