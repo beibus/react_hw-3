ad9f8cc981412073e6acd0eba428af236067197b
Ulukbek <orozgozhoev.u@gmail.com>
2022-08-12 13:41:20 +0600

    Ulukbek added details page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Details from './Details';
+import Footer from './Components/Footer/Footer';
+
+
+import SierraImage from './Images/Sierrapic.png'
+
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  
+  );
+}
+
+export default App;
